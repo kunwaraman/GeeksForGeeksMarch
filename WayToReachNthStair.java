e3c8dac05e8a334d02296f591b78dab5bbaeff2f
kunwaraman <amankunwar8472@gmail.com>
2025-03-11 22:07:13 +0530

    day11

---
+++ WayToReachNthStair.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/*
+time complexity: O(n)
+space complexity: O(n)
+ */
+public class WayToReachNthStair {
+    int dp[]= new int[46];
+    int countWays(int n) {
+        // your code here
+        Arrays.fill(dp,-1);
+        return recur(n);
+    }
+    int recur(int n){
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
